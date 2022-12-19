3f7d56760cecef24f465a99c069eb72b550cde66
Theodoro Gaspar Ferreira <theodoro.ferreira.pb@compasso.com.br>
2022-12-19 14:51:58 -0300

    Adicionando swagger-ui

--- src/main/java/br/com/pb/barbershop/msuser/framework/adapters/in/UserController.java
+++ src/main/java/br/com/pb/barbershop/msuser/framework/adapters/in/UserController.java
@@ -2,6 +2,8 @@ package br.com.pb.barbershop.msuser.framework.adapters.in;
 
 import br.com.pb.barbershop.msuser.application.service.UserService;
 import br.com.pb.barbershop.msuser.domain.dto.PageableDTO;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/user")
+@SecurityScheme(
+        name = "Bearer",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer"
+)
 public class UserController {
 
     private final UserService service;
