1481e62da0197660d3b0e76e2e7f192c83b08f6f
David <david.gieseler@hotmail.com>
2022-12-19 15:19:08 -0300

    Correção return controller delete

--- src/main/java/br/com/pb/barbershop/msuser/framework/adapters/in/UserController.java
+++ src/main/java/br/com/pb/barbershop/msuser/framework/adapters/in/UserController.java
@@ -4,6 +4,7 @@ import br.com.pb.barbershop.msuser.application.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -14,9 +15,10 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+
     @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    public void deleteUserId(@PathVariable Long id){userService.deleteUserId(id);}
-
+    public ResponseEntity deleteUserId (@PathVariable Long id){
+        userService.deleteUserId(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
-
